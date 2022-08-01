923406fddbf1f5ea6f41b1dc66ffe75b4dac7d3e
Nicole Lemos <niikii.ense@gmail.com>
2022-08-01 13:30:19 -0300

    Se agregaron eventos

--- main.js
+++ main.js
@@ -1,20 +1,15 @@
 const cart = []
+let subtotalCart = 0
 
-const products = [
-    {id: 1, title: 'Retén 22', price: 220},
-    {id: 2, title: 'Retén 25', price: 270},
+let products = [
+    {id: 1, title: 'Reten 22', price: 220},
+    {id: 2, title: 'Reten 25', price: 270},
     {id: 3, title: 'Ruleman SFK 6201', price: 580},
     {id: 4, title: 'Ruleman SFK 6202', price: 630},
     {id: 5, title: 'Ruleman SFK 6203', price: 630},
     {id: 6, title: 'Ruleman SFK 6204', price: 970},
     {id: 7, title: 'Soporte Drean 03 04', price: 4570}
 ]
-
-function addToCart(product){
-    cart.push(product)
-    console.log('Se agregó ' + product.title + ': $' + product.price + ' al carrito')
-    alert('Agregaste ' + product.title + ': $' + product.price + ' al carrito. Productos en carrito: ' + cart.length)
-}
 
 function deleteFromCart(productId) {
     const index = cart.findIndex((product) => product.id === productId)
@@ -25,23 +20,32 @@ function deleteFromCart(productId) {
     }
 }
 
-function search(){
-    let search = prompt('Buscar productos')
-    const searchResult = products.filter((product) => product.title.includes(search))
-    console.log(searchResult)
-} 
-
-console.log('Cantidad de productos en carrito: ' + cart.length)
-console.log(cart)
-
 products.forEach((product) =>{
+    let buttonId = `addToCart${product.id}`
     document.getElementById('productsCatalogue').innerHTML +=
     `<div class='productCard'>
     <p>${product.title}</p>
     <p>$${product.price}</p>
-    <button>Agregar al Carrito</button>
+    <button type="button" id="${buttonId}" class="btn btn-primary">Agregar al Carrito</button>
     </div>`
-    }
+}
 )
 
-document.getElementById('navBar').innerHTML = `<input placeholder="Buscar productos"> </input> <button>Buscar</button>`
+products.forEach((product) => {
+    let buttonId = `addToCart${product.id}`
+    document.getElementById(buttonId).addEventListener('click', () => {
+        subtotalCart = subtotalCart + product.price
+        cart.push(product)
+        console.log('Se agregó ' + product.title + ': $' + product.price + ' al carrito. Productos en carrito: ' + cart.length)
+        console.log('Total del carrito: $' + subtotalCart)
+    })
+})
+
+let search = document.getElementById('searchInput')
+
+search.addEventListener("input", e => {
+    let value = e.target.value.toLowerCase()
+    let searchResult = products.filter((product) => product.title.toLowerCase().includes(value))
+    console.log(value)
+    console.log(searchResult)
+})
