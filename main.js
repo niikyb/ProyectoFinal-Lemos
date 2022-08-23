17291f74fcf12ac8bfdfa84e98375c94fbc4878c
Nicole Lemos <niikii.ense@gmail.com>
2022-08-23 19:34:35 -0300

    agregado products.json

--- main.js
+++ main.js
@@ -26,25 +26,38 @@ document.getElementById('show-cart').addEventListener('click', () => {
     renderCart()
 })
 
-let products = [
-    {id: 1, title: 'Reten 22', price: 220},
-    {id: 2, title: 'Reten 25', price: 270},
-    {id: 3, title: 'Ruleman SFK 6201', price: 580},
-    {id: 4, title: 'Ruleman SFK 6202', price: 630},
-    {id: 5, title: 'Ruleman SFK 6203', price: 630},
-    {id: 6, title: 'Ruleman SFK 6204', price: 970},
-    {id: 7, title: 'Soporte Drean 03 04', price: 4570}
-]
+const renderCards = async () => {
+    const response = await fetch ('./products.json')
+    const data = await response.json ()
 
-products.forEach((product) =>{
-    let buttonId = `addToCart${product.id}`
-    document.getElementById('productsCatalogue').innerHTML +=
-    `<div class='productCard'>
-    <p>${product.title}</p>
-    <p>$${product.price}</p>
-    <button type="button" id="${buttonId}" class="btn btn-primary">Agregar al Carrito</button>
-    </div>`
-})
+    data.forEach((product) =>{
+        let buttonId = `addToCart${product.id}`
+        document.getElementById('productsCatalogue').innerHTML +=
+        `<div class='productCard'>
+        <p>${product.title}</p>
+        <p>$${product.price}</p>
+        <button type="button" id="${buttonId}" class="btn btn-primary">Agregar al Carrito</button>
+        </div>`
+        products.push (product)
+    })
+    data.forEach((product) => {
+        let buttonId = `addToCart${product.id}`
+        document.getElementById(buttonId).addEventListener('click', () => {
+            addToCart(product)
+            Toastify({
+                text: 'Agregaste ' + product.title + ' al carrito.',
+                offset: {
+                    y: 50
+                },
+                style: {
+                    background: 'linear-gradient(to right, #00b09b, #96c93d)',
+                }
+                }).showToast();
+        })
+    })
+}
+
+renderCards ()
 
 function addToCart (product){
     cart.push(product)
@@ -54,19 +67,3 @@ function addToCart (product){
     subtotalCart = subtotalCart + product.price
     localStorage.setItem('cartTotal', JSON.stringify(subtotalCart))
 }
-
-products.forEach((product) => {
-    let buttonId = `addToCart${product.id}`
-    document.getElementById(buttonId).addEventListener('click', () => {
-        addToCart(product)
-        Toastify({
-            text: 'Agregaste ' + product.title + ' al carrito.',
-            offset: {
-                y: 50
-            },
-            style: {
-                background: 'linear-gradient(to right, #00b09b, #96c93d)',
-            }
-            }).showToast();
-    })
-})
